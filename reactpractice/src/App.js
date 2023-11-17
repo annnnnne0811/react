5657b67eef22f0e655c6ae36ffcea2512d692676
Anne <maryanne_2000@yahoo.com>
2023-11-17 22:50:27 +0000

    react Practice

---
+++ reactpractice/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  const name = <h1>Anne</h1>
+  return (
+    <div className="App">
+      {name}
+      {name}
+
+
+    </div>
+  );
+}
+
+export default App;
